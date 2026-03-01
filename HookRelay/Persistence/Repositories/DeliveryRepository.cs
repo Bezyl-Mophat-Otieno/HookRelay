e4f3947e7bf3b64c74319ec46e1b1eb44af5c133
Bezyl Mophat Otieno <bezylmophatotieno@gmail.com>
2026-03-01 21:38:56 +0300

    Created a separate Queue for the deliveries and now writing to the Queue via a dedicated IEventProcessor

--- HookRelay/Persistence/Repositories/DeliveryRepository.cs
+++ HookRelay/Persistence/Repositories/DeliveryRepository.cs
@@ -1,6 +1,18 @@
+using HookRelay.Persistence.Models;
+
 namespace HookRelay.Persistence.Repositories;
 
-public class DeliveryRepository
-{
+public class DeliveryRepository(HookRelayDbContext dbContext)
+{    public async Task<bool> AddDeliveryAsync(Delivery newDelivery)
+    {
+        await dbContext.AddAsync(newDelivery);
+        return await dbContext.SaveChangesAsync() > 0;
+    }
+    public async Task<bool> AddDeliveryAsync(ICollection<Delivery> newDeliveries)
+    {
+        await dbContext.AddRangeAsync(newDeliveries);
+        return await dbContext.SaveChangesAsync() > 0;
+    }
+    
     
 }
--- HookRelay/Services/EventDispatcherWorker.cs
+++ HookRelay/Services/EventDispatcherWorker.cs
@@ -1,6 +1,7 @@
 using System.Threading.Channels;
 using HookRelay.Persistence;
 using HookRelay.Persistence.Models;
+using HookRelay.Services.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 namespace HookRelay.Services;
@@ -15,11 +16,13 @@ public class EventDispatcherWorker(Channel<Event>channel, IServiceScopeFactory s
         {
             await foreach (var evt in channel.Reader.ReadAllAsync(stoppingToken))
             {
+                var scope = scopeFactory.CreateScope();
+                var eventProcessor = scope.ServiceProvider.GetRequiredService<IEventProcessor>();
                 logger.LogInformation("EventDispatcherWorker started.");
                 try
                 {
                     logger.LogInformation("Processing Event {EventType} Created on {CreatedAt:yy-MMM-dd ddd}", evt.EventType, evt.CreatedAt);
-                    await ProcessEvent(evt, stoppingToken);
+                    await eventProcessor.ProcessEventAsync(evt, stoppingToken);
                 }
                 catch (Exception e)
                 {
@@ -29,35 +32,5 @@ public class EventDispatcherWorker(Channel<Event>channel, IServiceScopeFactory s
         }
             
     }
-
-    private async Task ProcessEvent(Event evt, CancellationToken token)
-    {
-        var scope = scopeFactory.CreateScope();
-        var db = scope.ServiceProvider.GetRequiredService<HookRelayDbContext>();
-        var webhooks = await db.Webhooks.Where(whk => whk.EventType == evt.EventType).ToListAsync(token);
-        logger.LogInformation("Dispatching {Count} webhooks for {EventId}", webhooks.Count, evt.EventId);
-        var tasks = webhooks.Select( async webhook => await HandleDelivery(webhook, evt));
-        await Task.WhenAll(tasks);
-    }
-
-    private async Task HandleDelivery(Webhook webhook, Event evt)
-    {
-        var attempt = 1;
-        while (attempt <= _maxRetries )
-        {
-            try
-            {
-                logger.LogInformation("Attempting delivery to webhook url: {webhookUrl}", webhook.Url);
-                var response = await _httpClient.PostAsJsonAsync(webhook.Url, evt.Payload);
-                if (response.IsSuccessStatusCode) return;
-                attempt++;
-            }
-            catch
-            {
-                attempt++;
-            }
-            
-        }
-        logger.LogInformation("EventId: {eventId} of EventType: {eventType} could not be delivered to the webhook url: {webhookUrl} after a maximum number of {attempts} attempts", evt.EventId, evt.EventType, webhook.Url, attempt);
-    }
+    
 }
